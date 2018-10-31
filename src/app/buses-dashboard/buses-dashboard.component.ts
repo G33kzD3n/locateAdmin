c0d623f481857a5d197e6caef6a3435457a74cd3
Sami Ullah <muftisami2010@gmail.com>
2018-10-31 19:11:54 +0530

    merged with nadeems apis

--- src/app/buses-dashboard/buses-dashboard.component.ts
+++ src/app/buses-dashboard/buses-dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BusesDashboardService } from '../buses-dashboard/services/buses-dashboard.service';
+import { RequestOptions, Headers } from '@angular/http';
 import { Router } from '@angular/router'
+import { Validators, FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-buses-dashboard',
@@ -10,13 +12,25 @@ import { Router } from '@angular/router'
 export class BusesDashboardComponent implements OnInit {
   buses: any;
   stops: any;
-  constructor(protected busSer: BusesDashboardService, protected router: Router) { }
+  busForm: FormGroup;
+  constructor(protected fb: FormBuilder, protected busSer: BusesDashboardService, protected router: Router) { }
 
   ngOnInit() {
     if (localStorage.getItem('loggedIn') !== 'true') {
       this.router.navigate(['login']);
     }
     this.getBuses();
+    this.busForm = this.fb.group({
+      busno: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/^[0-9]*$/),
+        Validators.maxLength(6),
+        Validators.minLength(4),
+      ])],
+      gpsid: ['', Validators.compose([
+        Validators.required,
+      ])],
+    });
   }
 
   getBuses() {
@@ -25,8 +39,6 @@ export class BusesDashboardComponent implements OnInit {
         res => {
           this.buses = res.buses;
           this.stops = res.buses[0].stops.names.split(';');
-          console.log(this.buses);
-          console.log(this.stops);
 
         },
         err => {
@@ -39,5 +51,61 @@ export class BusesDashboardComponent implements OnInit {
         }
       );
   }
+  editBus(bus_no: number) {
+    let token = localStorage.getItem('token');
+    // let headers = new Headers({ 'Content-Type': 'application/json' });
+    // headers.append('Authorization', 'Bearer ' + token);
+    let headers = new Headers({ 'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': 'Bearer ' + token });
+    let options = new RequestOptions({ headers: headers });
+    this.busSer.editBus(bus_no, options)
+      .subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log(err);
+          if (err.status == 0) {
+            alert("Check your Internet connection");
+          }
+        }
+      );
+  }
+  addBus(busForm) {
+    let payload = {
+      bus_no: this.busForm.controls['busno'].value,
+      gps_id: this.busForm.controls['gpsid'].value
+    };
+    // let headers = new Headers({ 'Content-Type': 'application/json' });
+    // headers.append('Authorization', 'Bearer ' +localStorage.getItem('token'));
+    // let headers = new Headers({'Content-Type':'application/json','Authorization':'Bearer '+localStorage.getItem('token')});
+    //let options = new RequestOptions({ headers: headers });
+    this.busSer.addBus(payload)
+      .subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log(err);
+          if (err.status == 0) {
+            alert("Check your Internet connection");
+          }
+        }
+      );
+  }
+  deleteBus(bus_no) {
+    let headers = new Headers({ 'Content-Type': 'application/json', 'Accept': 'application/json', 'Authorization': 'Bearer ' + localStorage.getItem('token') });
+    let options = new RequestOptions({ headers: headers });
+    this.busSer.deleteBus(bus_no, options)
+      .subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log(err);
+          if (err.status == 0) {
+            alert("Check your Internet connection");
+          }
+        }
+      );
+  }
 }
-  
--- src/app/buses-dashboard/services/buses-dashboard.service.ts
+++ src/app/buses-dashboard/services/buses-dashboard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AppService } from '../../app.service';
 
@@ -12,6 +12,23 @@ export class BusesDashboardService {
   constructor(protected http: HttpClient, protected app: AppService) { }
 
   getBuses(): Observable<any> {
-    return this.http.get(this.app.baseUrl + '/buses');
+    return this.http.get(this.app.baseUrl2 + '/buses');
+  }
+  editBus(bus_no, options): Observable<any> {
+    console.log(options);
+    return this.http.put(this.app.baseUrl + '/buses/' + bus_no + '/edit', options);
+  }
+  addBus(payload): Observable<any> {
+    // console.log(options);
+    let token =localStorage.getItem('token');
+    return this.http.post(this.app.baseUrl + '/buses/create', payload, {
+        headers: new HttpHeaders({
+        'Authorization': 'Bearer ' +token,
+        'Accept': 'application/json'
+      })
+    });
+  }
+  deleteBus(bus_no, options): Observable<any> {
+    return this.http.delete(this.app.baseUrl + '/buses/' + bus_no + '/delete', options);
   }
 }
